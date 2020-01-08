6dfda1c8a77e49d5be16c8fe4211730808cdd8c2
Sanders MARTINE <sanders.martine9@gmail.com>
2020-01-08 15:06:37 +0100

    create project

---
+++ app/src/main/java/clean/archi/template/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package clean.archi.template.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import clean.archi.template.R
+
+
+class MainActivity : AppCompatActivity()
+{
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
